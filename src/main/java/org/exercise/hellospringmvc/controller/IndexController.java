74b955f686f22635af52a4e9df101bcefa2fbfce
xgiannix <gianni.demare@hotmail.it>
2024-01-03 15:26:33 +0100

    Initial commit

---
+++ src/main/java/org/exercise/hellospringmvc/controller/IndexController.java
@@ -0,0 +1,27 @@
+package org.exercise.hellospringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping("/")
+    public String index(@RequestParam(name = "dice", defaultValue = "6", required = false) int dice, Model model) {
+        // GENERA NUMERO CASUALE
+        Random random = new Random();
+        int randomNumber = random.nextInt(dice) + 1;
+
+        // AGGIUNGO ATTRIBUTO
+        model.addAttribute("number", randomNumber);
+        return "home";
+    }
+}
+
+
+
